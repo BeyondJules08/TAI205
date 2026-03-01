2ca18713f581326efbdd43c8102833e2af2c222a
BeyondJules08 <jsvcbusiness08@gmail.com>
2026-02-28 21:44:06 -0800

    pydantic
    import of pydantic, create a model with the library and used in the post endpoint

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -2,6 +2,7 @@
 from fastapi import FastAPI,status,HTTPException
 from typing import Optional
 import asyncio
+from pydantic import BaseModel, Field
 
 #2. Inicializacion de la APP
 app = FastAPI(
@@ -15,6 +16,11 @@ usuarios=[
     {"id":"2", "nombre":"Eliseo", "apellidos":"Novas", "edad":"20"},
     {"id":"3", "nombre":"Sacatripas", "apellidos":"Ripper", "edad":"30"},
 ]
+#Creamos el modelo
+class crear_usuario(BaseModel):
+    id:int = Field(..., gt=0, description="ID del usuario")
+    nombre:str = Field(..., min_length=3, max_length=50, example="Julian")
+    edad:int = Field(..., ge=1, le=123, description="Edad del usuario")
 
 #3. Endpoints
 @app.get("/", tags=["Inicio"])
@@ -47,9 +53,9 @@ async def consulta_usuarios():
     }
 #POST
 @app.post("/v1/usuario/", tags=["CRUD HTTP"])
-async def crea_usuario(usuario:dict):
+async def crear_usuario(usuario:crear_usuario): #modelo
     for usr in usuarios:
-        if usr["id"] == usuario.get("id"):
+        if usr["id"] == usuario.id: #validacion de id unico
             raise HTTPException(
                 status_code=400,
                 detail=" El id ya existe"
