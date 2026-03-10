ed44bf076d0fa568d53e9aa923a78ca856f7ec9d
BeyondJules08 <jsvcbusiness08@gmail.com>
2026-03-09 18:50:46 -0700

    1er Commit

---
+++ Examen2do/app/main.py
@@ -0,0 +1,101 @@
+from fastapi import FastAPI,status,HTTPException, Depends # HTTPException para errores, Depends para dependencias
+from typing import Optional
+import asyncio
+from pydantic import BaseModel, Field
+from fastapi.security import HTTPBasic, HTTPBasicCredentials # HTTPBasic para autenticacion basica, HTTPBasicCredentials para credenciales
+import secrets # Para comparar credenciales
+
+app = FastAPI(
+    title="API Sistema de Reservas Hospedaje",
+    description="Julian Santiago",
+    version="1.0.0",
+    )
+
+# BD Ficticia
+reservas=[
+    {"id":1, "huesped":"Julian", "fecha_de_entrada:":"17:20", "fecha_de_salida":"20:00","tipo_habitacion":"sencilla", "dias_estancia":1, "estado_reserva":"confirmado"},
+    {"id":2, "huesped":"Eliseo", "fecha_de_entrada":"19:50", "fecha_de_salida":"22:00","tipo_habitacion":"doble", "dias_estancia":2, "estado_reserva":"pendiente"},
+    {"id":3, "huesped":"Sacatripas", "fecha_de_entrada":"20:40", "fecha_de_salida":"23:00","tipo_habitacion":"suite", "dias_estancia":3, "estado_reserva":"cancelado"}
+]
+
+class huesped(BaseModel):
+    id:int = Field(..., gt=0, description="ID del huesped")
+    huesped:str = Field(..., min_length=5, max_length=50, example="Julian")
+    fecha_de_entrada:str = Field(..., ge=1, le=123, description="Fecha de entrada no menor a fecha actual")
+    fecha_de_salida:str = Field(..., ge=1, le=123, description="Fecha de salida mayor que fecha de entrada")
+    tipo_habitacion:str = Field(..., min_length=5, max_length=8, example="sencilla")
+    dias_estancia:int = Field(..., ge=1, le=7, description="Dias de estancia")
+    estado_reserva:str = Field(..., )
+    
+seguridad = HTTPBasic()
+def verificar_peticion(credenciales: HTTPBasicCredentials = Depends(seguridad)):
+    userAuth = secrets.compare_digest(credenciales.username, "hotel")
+    passAuth = secrets.compare_digest(credenciales.password, "r2026")
+    if not (userAuth and passAuth):
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales invalidas",
+        )
+    return credenciales.username
+
+# Crear reservas POST
+@app.post("/v1/reservas/", tags=["CRUD HTTP"])
+async def crear_reserva(reserva:crear_reserva, userAuth:str = Depends(verificar_peticion)):
+    for rev in reservas:
+        if rev["id"] == reserva.id: #validacion de id unico
+            raise HTTPException(
+                status_code=400,
+                detail=" El id ya existe"
+            )
+    reservas.append(reserva)
+    return {
+        "mensaje": "Reserva agregada correctamente",
+        "status" : "200",
+        "reserva":reserva
+    }
+    
+# Listar reservas GET
+@app.get("/v1/reservas/", tags=["CRUD HTTP"])
+async def consulta_reservas():
+    return{
+        "status":"200",
+        "total": len(reservas),
+        "data": reservas
+    }
+    
+# Consultar por ID GET
+@app.get("/v1/reservas{id}", tags=["CRUD HTTP"])
+async def consulta_uno(id: int):
+    return {"Resultado":"Reserva encontrada",
+            "estatus":"200"
+            }
+    
+# Confirmar reserva PUT
+@app.put("/v1/reservas/{id}", tags=["CRUD HTTP"])
+async def actualiza_reserva(id: str, usuario:dict):
+    for rev in usuarios:
+        if rev["id"] == id:
+            rev["id"] = reserva.get("id")
+            rev["huesped"] = reserva.get("nombre")
+            rev["fecha_entrada"] = reserva.get("apellidos")
+            rev["fecha_salida"] = reserva.get("edad")
+            rev["tipo_habitacion"] = reserva.get("edad")
+            rev["dias_estancia"] = reserva.get("edad")
+            rev["estado_reserva"] = reserva.get("edad")
+            return {
+                "mensaje": "usuario actualizado correctamente",
+                "status" : "200",
+                "reserva": reserva
+            }
+            
+# Cancelar reserva DELETE
+@app.delete("/v1/reservas/{id}", tags=["CRUD HTTP"])
+async def elimina_reserva(id: int, userAuth:str = Depends(verificar_peticion)):
+    for rev in reservas:
+        if rev["id"] == id:
+            reservas.remove(rev)
+            return {
+                "mensaje": f" eliminado por {userAuth}",
+                "status" : "200",
+                "reserva": reserva
+            }
